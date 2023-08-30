fd1a25fdf5899d1c65761ee3c232b88a4ad7b2e6
Sowndharya <sowndharyakandhavel08@gmail.com>
2023-08-30 12:16:07 +0530

    Village  development works Animation changes

--- lib/Activity/Auth/Village_development_works.dart
+++ lib/Activity/Auth/Village_development_works.dart
@@ -282,116 +282,121 @@ class _VillagedevelopmentState extends State<Villagedevelopment> {
   }
 
   Widget workDetailsWidget() {
-    return Column(
+    return
+      Column(
       children: [
-        UIHelper.titleTextStyle('work_details'.tr().toString(), c.primary_text_color2, 14, false, true),
+        UIHelper.titleTextStyle(workitems[selectedIndex]['name'].toString(), c.primary_text_color2, 14, false, true),
         Container(
-          child: ListView.builder(
-            physics: NeverScrollableScrollPhysics(),
-            shrinkWrap: true,
-            itemCount: items.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Container(
-                margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
-                decoration: UIHelper.roundedBorderWithColorWithShadow(25, c.need_improvement1, c.need_improvement1, borderColor: Colors.transparent, borderWidth: 0),
-                child: AnimationConfiguration.staggeredList(
-                  position: index,
-                  duration: const Duration(milliseconds: 800),
-                  child: SlideAnimation(
-                    horizontalOffset: 200.0,
-                    child: FlipAnimation(
-                        child: Column(
-                      children: [
-                        Stack(children: [
-                          GestureDetector(
-                              onTap: () {
-                                setState(() {
-                                  if (showFlag.contains(index)) {
-                                    showFlag.remove(index);
-                                  } else {
-                                    showFlag.add(index);
-                                  }
-                                });
-                              },
-                              child: Container(
-                                height: 110,
-                                padding: EdgeInsets.only(left: 8),
-                                decoration: UIHelper.roundedBorderWithColorWithShadow(25, c.white, c.white, borderColor: Colors.transparent, borderWidth: 0),
-                                child: Row(
-                                  children: [
-                                    Container(
-                                      width: 50,
-                                      decoration: BoxDecoration(color: c.white, borderRadius: BorderRadius.only(topLeft: Radius.circular(20), bottomLeft: Radius.circular(25))),
-                                      child: Text(
-                                        items[index],
-                                        textAlign: TextAlign.center,
-                                      ),
-                                    ),
-                                    Expanded(
-                                        child: Container(
-                                            decoration: BoxDecoration(color: c.colorPrimary, borderRadius: BorderRadius.only(topRight: Radius.circular(20), bottomRight: Radius.circular(25))),
-                                            child: Stack(
-                                              children: [
-                                                Container(
-                                                  margin: EdgeInsets.only(top: 8),
-                                                  transform: Matrix4.translationValues(-10, 0, 0),
-                                                  alignment: Alignment.topLeft,
-                                                  child: Image.asset(
-                                                    imagePath.rightarrow,
-                                                    height: 25,
-                                                    width: 30,
-                                                    color: c.white,
-                                                  ),
-                                                ),
-                                                Container(
-                                                  margin: EdgeInsets.only(top: 30, left: 15, right: 10),
-                                                  child: Column(
-                                                    mainAxisAlignment: MainAxisAlignment.start,
-                                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                                    children: [
-                                                      Row(
-                                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                                        children: [
-                                                          UIHelper.titleTextStyle('Scheme Name : ', c.white, 12, false, false),
-                                                          UIHelper.titleTextStyle('MGNREGS', c.white, 12, true, false),
-                                                        ],
-                                                      ),
-                                                      UIHelper.verticalSpaceSmall,
-                                                      Row(
-                                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                                        children: [
-                                                          UIHelper.titleTextStyle('Work Type Name : ', c.white, 12, false, false),
-                                                          Expanded(child: UIHelper.titleTextStyle('Library Building Repair and Toilet Construction', c.white, 12, true, false)),
-                                                        ],
-                                                      ),
-                                                    ],
-                                                  ),
-                                                ),
-                                                Container(
-                                                    margin: EdgeInsets.only(right: 5, bottom: 5),
-                                                    alignment: Alignment.bottomRight,
-                                                    child: Icon(
-                                                      showFlag.contains(index) ? Icons.expand_less_outlined : Icons.expand_more_outlined,
-                                                      color: c.white,
-                                                    )),
-                                              ],
-                                            ))),
-                                  ],
-                                ),
-                              )),
-                          Container(
-                            margin: EdgeInsets.all(10.0),
-                            color: c.full_transparent,
-                          ),
-                        ]),
-                        expandedCardWidget(context, index)
-                      ],
-                    )),
-                  ),
-                ),
-              );
-            },
-          ),
+         child: AnimationLimiter(
+           key: ValueKey(selectedIndex),
+           child: ListView.builder(
+             physics: NeverScrollableScrollPhysics(),
+             shrinkWrap: true,
+             itemCount: items.length,
+             itemBuilder: (BuildContext context, int index) {
+             return AnimationConfiguration.staggeredList(
+                 position: index,
+                 duration: const Duration(milliseconds: 800),
+                 child:SlideAnimation(
+                   horizontalOffset: 200.0,
+                   child:FlipAnimation(
+                       child: Container(
+                           margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
+                           decoration: UIHelper.roundedBorderWithColorWithShadow(25, c.need_improvement1, c.need_improvement1, borderColor: Colors.transparent, borderWidth: 0),
+                       child: Column(
+                         children: [
+                           Stack(children: [
+                             GestureDetector(
+                                 onTap: () {
+                                   setState(() {
+                                     if (showFlag.contains(index)) {
+                                       showFlag.remove(index);
+                                     } else {
+                                       showFlag.add(index);
+                                     }
+                                   });
+                                 },
+                                 child: Container(
+                                   height: 110,
+                                   padding: EdgeInsets.only(left: 8),
+                                   decoration: UIHelper.roundedBorderWithColorWithShadow(25, c.white, c.white, borderColor: Colors.transparent, borderWidth: 0),
+                                   child: Row(
+                                     children: [
+                                       Container(
+                                         width: 50,
+                                         decoration: BoxDecoration(color: c.white, borderRadius: BorderRadius.only(topLeft: Radius.circular(20), bottomLeft: Radius.circular(25))),
+                                         child: Text(
+                                           items[index],
+                                           textAlign: TextAlign.center,
+                                         ),
+                                       ),
+                                       Expanded(
+                                           child: Container(
+                                               decoration: BoxDecoration(color: c.colorPrimary, borderRadius: BorderRadius.only(topRight: Radius.circular(20), bottomRight: Radius.circular(25))),
+                                               child: Stack(
+                                                 children: [
+                                                   Container(
+                                                     margin: EdgeInsets.only(top: 8),
+                                                     transform: Matrix4.translationValues(-10, 0, 0),
+                                                     alignment: Alignment.topLeft,
+                                                     child: Image.asset(
+                                                       imagePath.rightarrow,
+                                                       height: 25,
+                                                       width: 30,
+                                                       color: c.white,
+                                                     ),
+                                                   ),
+                                                   Container(
+                                                     margin: EdgeInsets.only(top: 30, left: 15, right: 10),
+                                                     child: Column(
+                                                       mainAxisAlignment: MainAxisAlignment.start,
+                                                       crossAxisAlignment: CrossAxisAlignment.start,
+                                                       children: [
+                                                         Row(
+                                                           crossAxisAlignment: CrossAxisAlignment.start,
+                                                           children: [
+                                                             UIHelper.titleTextStyle('Scheme Name : ', c.white, 12, false, false),
+                                                             UIHelper.titleTextStyle('MGNREGS', c.white, 12, true, false),
+                                                           ],
+                                                         ),
+                                                         UIHelper.verticalSpaceSmall,
+                                                         Row(
+                                                           crossAxisAlignment: CrossAxisAlignment.start,
+                                                           children: [
+                                                             UIHelper.titleTextStyle('Work Type Name : ', c.white, 12, false, false),
+                                                             Expanded(child: UIHelper.titleTextStyle('Library Building Repair and Toilet Construction', c.white, 12, true, false)),
+                                                           ],
+                                                         ),
+                                                       ],
+                                                     ),
+                                                   ),
+                                                   Container(
+                                                       margin: EdgeInsets.only(right: 5, bottom: 5),
+                                                       alignment: Alignment.bottomRight,
+                                                       child: Icon(
+                                                         showFlag.contains(index) ? Icons.expand_less_outlined : Icons.expand_more_outlined,
+                                                         color: c.white,
+                                                       )),
+                                                 ],
+                                               ))),
+                                     ],
+                                   ),
+                                 )),
+                             Container(
+                               margin: EdgeInsets.all(10.0),
+                               color: c.full_transparent,
+                             ),
+                           ]),
+                           expandedCardWidget(context, index)
+                         ],
+                       )
+                   )
+                 )
+                 )
+             );
+             },
+           ),
+         ),
         )
       ],
     );
